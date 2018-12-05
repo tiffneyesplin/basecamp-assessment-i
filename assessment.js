ea96a932551c749a0f5572930b36b1a36bed34dc
Tiffney Esplin <tiffneyesplin@Tiffneys-MacBook-Pro.local>
2018-12-05 10:06:52 -0700

    initial commit

--- assessment.js
+++ assessment.js
@@ -1,6 +1,6 @@
 // #1 create a variable called 'myName' and assign it to your name as a string
 
-// CODE HERE
+var myName = "Tiffney Esplin"
 
 // #2 create a variable called 'faveNum' and assign it to your favorite number
 
